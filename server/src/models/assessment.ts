4a733647022fa207a0751e34f88d7e177cf2dad5
wwong4374 <wilson.ka.wong@gmail.com>
2025-04-25 21:18:44 -1000

    Add backend assessment models

---
+++ server/src/models/assessment.ts
@@ -0,0 +1,31 @@
+export enum Domain {
+  Depression = 'DEPRESSION',
+  Mania = 'MANIA',
+  Anxiety = 'ANXIETY',
+  SubstanceUse = 'SUBSTANCE USE'
+}
+
+enum Level2Assessment {
+  Phq9 = 'PHQ-9',
+  Asrm = 'ASRM',
+  Assist = 'ASSIST',
+}
+
+export interface Answer {
+  questionId: string,
+  value: number,
+}
+
+export const DomainToScoreThreshold: Record<Domain, number> = {
+  [Domain.Depression]: 2,
+  [Domain.Mania]: 2,
+  [Domain.Anxiety]: 2,
+  [Domain.SubstanceUse]: 1,  
+}
+
+export const DomainToLevel2Assessment: Record<Domain, Level2Assessment> = {
+  [Domain.Depression]: Level2Assessment.Phq9,
+  [Domain.Mania]: Level2Assessment.Asrm,
+  [Domain.Anxiety]: Level2Assessment.Phq9,
+  [Domain.SubstanceUse]: Level2Assessment.Assist,
+}
